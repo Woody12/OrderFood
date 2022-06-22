1dce8061ebf586402e08d64b39a3491afb6dd479
Woody12 <wulee12@gmail.com>
2022-06-22 00:31:25 -0700

    Initial commit

---
+++ Assets/OrderFood.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Sprites;
+using TMPro;
+
+public class OrderFood : MonoBehaviour
+{
+    [SerializeField] Sprite _food1, _food2, _food3, _food4;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
