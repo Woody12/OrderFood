c10bf15982b5d9522926a96adf410267144bccc2
MindWarpProd <99101069+MindWarpProd@users.noreply.github.com>
2022-06-29 11:56:25 -0700

    Added a simple scene, removed the buttons from the Holster, changed POS of EndDialog
    Added a simple scene to the view so that it looks as if we are playing a game. Also I removed the buttons from the under Dash Foods and added them to their Own Canvas. This way they are not manipulated when you move the Food Order canvas, this was causing issue with the button getting in the way and was just not looking right(they are now stationary). Adjusted the particle system to the button. Moved Dialog Box to be centered in the Holster bounding control box.

--- Assets/Scripts/OrderFoodScripts/HungryButton.cs
+++ Assets/Scripts/OrderFoodScripts/HungryButton.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class HungryButton : MonoBehaviour
 {
-    [SerializeField] GameObject _hungryPanel, _hungryImageOne, _hungryImageTwo, _menu, _endDialog;
+    [SerializeField] GameObject _hungryPanel, _hungryImageOne, _hungryImageTwo, _menu, _endDialog, _dashHolster;
     // Start is called before the first frame update
     void Start()
     {
@@ -37,17 +37,30 @@ public class HungryButton : MonoBehaviour
     IEnumerator Delay()
     {
         yield return new WaitForSeconds(1.5f);
-        _hungryPanel.SetActive(true);
-        _hungryImageOne.SetActive(true);
-        _hungryImageTwo.SetActive(true);
-        //hide the game objects
-        if (_menu.activeSelf)
+        if (_menu == null)
         {
+            Debug.LogError("HungryButton::_menu is null");
+        }
+        else if (!_menu.activeSelf)
+        {
+            _hungryPanel.SetActive(true);
+            _hungryImageOne.SetActive(true);
+            _hungryImageTwo.SetActive(true);
+        }
+        //hide the game objects
+        else if (_menu.activeSelf)
+        {
+            // there would be methods to send the data to the different delivery sevices
+            _hungryPanel.transform.localScale = new Vector3(0, 0, 0);
+            //_hungryImageOne.transform.localScale = new Vector3(0, 0, 0);
+            //_hungryImageTwo.transform.localScale = new Vector3(0, 0, 0);
             _menu.transform.localScale = new Vector3(0, 0, 0);
-            _hungryImageOne.transform.localScale = new Vector3(0, 0, 0);
-            _hungryImageTwo.transform.localScale = new Vector3(0, 0, 0);
             _endDialog.SetActive(true);
 
+        }
+        else
+        {
+            Debug.LogError("HungryButton::_menu active not set");
         }
         this.gameObject.transform.localScale = new Vector3(0, 0, 0);
     }
